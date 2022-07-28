ea3e0fb37c8b3d7f181f6f7c2bf3b4100e68654c
Cintulova <eliska.cintulova@capgemini.com>
2022-07-28 07:57:40 +0200

    first commit

---
+++ src/main/kotlin/com/tardigrada/WhateverAppMaven/WhateverAppMavenApplication.kt
@@ -0,0 +1,11 @@
+package com.tardigrada.WhateverAppMaven
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhateverAppMavenApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhateverAppMavenApplication>(*args)
+}
---
+++ src/test/kotlin/com/tardigrada/WhateverAppMaven/WhateverAppMavenApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tardigrada.WhateverAppMaven
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhateverAppMavenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
