c9ed24fe630b7c21f570abca0579af10529f477e
Cintulova <eliska.cintulova@capgemini.com>
2022-08-15 13:09:06 +0200

    tests for method saveUser from UserService almost finished

--- src/test/kotlin/com/tardigrada/WhateverAppMaven/service/UserServiceTest.kt
+++ src/test/kotlin/com/tardigrada/WhateverAppMaven/service/UserServiceTest.kt
@@ -6,7 +6,6 @@ import com.tardigrada.WhateverAppMaven.repository.UserRepository
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.springframework.boot.test.context.SpringBootTest
-import org.testng.Assert
 import org.testng.Assert.*
 import org.testng.annotations.Test
 import java.time.LocalDate
@@ -44,12 +43,86 @@ class UserServiceTest {
     }
 
     @Test
-    fun `should return NoSuchElementException when there is no saved user`() {
+    fun `should throw NoSuchElementException when there is no saved user`() {
         // given
         `when`(mockUserRepository.findById(0)).thenReturn(Optional.empty())
 
         // when & then
-        expectThrows(NoSuchElementException::class.java, { userService.getUserById(0) })
+        expectThrows(NoSuchElementException::class.java) { userService.getUserById(0) }
     }
+    
+    @Test
+    fun `should create new user`() {
+        // given
+        val userId = 0
+        val firstName = "Kate"
+        val lastName = "Green"
+        val email = "green@gmail.com"
+        val dateOfBirth = LocalDate.of(1986, 4, 8)
+        val street = "Long"
+        val city = "London"
+        val postcode = "E1 6AN"
+        val telephoneNumber = "+447911123456"
+        val user = User(userId, firstName, lastName, email, dateOfBirth, street, city, postcode, telephoneNumber)
+
+        `when`(mockInputValidator.inputCheck(firstName, lastName, email, dateOfBirth, street, city, postcode,
+            telephoneNumber)).thenReturn(true)
+        `when`(mockInputValidator.emailCheck(email)).thenReturn(true)
+        `when`(mockUserRepository.save(user)).thenReturn(user)
+        
+        // when
+        val savedUser = userService.saveUser(user)
+        
+        // then
+        assertEquals(savedUser, user)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when there is an incorrect format of email while creating new user`() {
+        // given
+        val userId = 0
+        val firstName = "Kate"
+        val lastName = "Green"
+        val email = "greengmail.com"
+        val dateOfBirth = LocalDate.of(1986, 4, 8)
+        val street = "Long"
+        val city = "London"
+        val postcode = "E1 6AN"
+        val telephoneNumber = "+447911123456"
+        val user = User(userId, firstName, lastName, email, dateOfBirth, street, city, postcode, telephoneNumber)
+
+        `when`(mockInputValidator.inputCheck(firstName, lastName, email, dateOfBirth, street, city, postcode,
+            telephoneNumber)).thenReturn(true)
+        `when`(mockInputValidator.emailCheck(email)).thenReturn(false)
+        `when`(mockUserRepository.save(user)).thenReturn(user)
+
+        // when & then
+        expectThrows(IllegalArgumentException::class.java) { userService.saveUser(user) }
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when there is blank variable while creating new user`() {
+        // given
+        val userId = 0
+        val firstName = "Kate"
+        val lastName = "Green"
+        val email = "green@gmail.com"
+        val dateOfBirth = LocalDate.of(1986, 4, 8)
+        val street = ""
+        val city = "London"
+        val postcode = "E1 6AN"
+        val telephoneNumber = "+447911123456"
+        val user = User(userId, firstName, lastName, email, dateOfBirth, street, city, postcode, telephoneNumber)
+
+        `when`(mockInputValidator.inputCheck(firstName, lastName, email, dateOfBirth, street, city, postcode,
+            telephoneNumber)).thenReturn(false)
+        `when`(mockInputValidator.emailCheck(email)).thenReturn(true)
+        `when`(mockUserRepository.save(user)).thenReturn(user)
+
+        // when & then
+        expectThrows(IllegalArgumentException::class.java) { userService.saveUser(user) }
+    }
+
+
 
 }
